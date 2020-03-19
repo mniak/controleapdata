a9a11abfa8f23595674601dc3a62c87dd62d11de
Andre Soares <andregsilv@gmail.com>
2020-03-19 01:21:51 -0300

    use microsoft dependency injection

--- app/Program.cs
+++ app/Program.cs
@@ -1,39 +1,44 @@
 ﻿using CommandLine;
 using ApdataTimecardFixer.Client;
-using PowerArgs;
-using SimpleInjector;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ApdataTimecardFixer
 {
-    class Program
+    public class Program
     {
         static Task<int> Main(string[] args)
         {
             return CommandLine.Parser.Default.ParseArguments<Arguments>(args)
                 .MapResult(
-                    async x => await RunWithArguments(x), 
+                    async x => await Run(x, log => {
+                        log.AddConsole();
+                    }), 
                           errs => Task.FromResult(1));
         }
 
-        public static async Task<int> RunWithArguments(Arguments arguments)
+        public static async Task<int> Run(Arguments arguments, Action<ILoggingBuilder> logBuilder)
         {
-            var container = new Container();
-            container.Register<Worker>();
-            container.RegisterSingleton(() => new CookieContainer()); container.Register(() =>
-            {
-                var handler = new HttpClientHandler();
-                handler.CookieContainer = container.GetInstance<CookieContainer>();
-                return handler;
-            });
-            container.Register(() => arguments);
-            container.Register(() => new HttpClient(container.GetInstance<HttpClientHandler>()));
-            container.Register<ApdataLowLevelClient>();
-            container.Register<ApdataClient>();
-
-            var worker = container.GetInstance<Worker>();
+            var worker = new ServiceCollection()
+                .AddScoped<Worker>()
+                .AddSingleton(svc=> new CookieContainer())
+                .AddScoped(svc =>
+                {
+                    var handler = new HttpClientHandler();
+                    handler.CookieContainer = svc.GetRequiredService<CookieContainer>();
+                    return handler;
+                })
+                .AddLogging(logBuilder)
+                .AddScoped(svc => arguments)
+                .AddScoped(svc => new HttpClient(svc.GetRequiredService<HttpClientHandler>()))
+                .AddScoped<ApdataLowLevelClient>()
+                .AddScoped<ApdataClient>()
+                .BuildServiceProvider()
+                .GetRequiredService<Worker>();
             await worker.Work();
             return 0;
         }
